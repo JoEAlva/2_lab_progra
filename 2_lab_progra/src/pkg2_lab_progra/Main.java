282ac2d8dc6d2099817ba05fee774e38a12713df
JorgeIgnacio <JorgeIgnacio@Jupiter>
2016-06-23 13:42:03 -0600

    2_lab_progra_conexion a hithub

---
+++ 2_lab_progra/src/pkg2_lab_progra/Main.java
@@ -0,0 +1,19 @@
+/*
+ * @Autor Jorge Ignacio Elizondo Alvarado
+ */
+package pkg2_lab_progra;
+
+/**
+ *
+ * @author JorgeIgnacio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
